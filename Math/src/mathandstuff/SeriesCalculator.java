8dd1c122ebde8942a33888fe442f032bcac49bcb
Chuck Ford <chuck.ford@kobie.com>
2020-06-18 17:45:44 -0400

    Created a new interface and modified Fibbonacci to use it

---
+++ Math/src/mathandstuff/SeriesCalculator.java
@@ -0,0 +1,12 @@
+package mathandstuff;
+
+public interface SeriesCalculator {
+	
+	/**
+	 * will return an array of first through the numberOfElements of a series 
+	 * @param numberOfElements
+	 * @return
+	 */
+	public int[] getSeries(int numberOfElements);
+
+}
